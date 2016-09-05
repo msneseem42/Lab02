89a7c576b5d717c58a154d82fd5f5bd2b8ff26aa
Bryan Nde <btnde42@students.tntech.edu>
2016-09-05 00:31:24 -0500

    your comments for the commit

--- ReadFile.h
+++ ReadFile.h
@@ -6,17 +6,21 @@
 #include <fstream>
 using namespace std;
 
-struct ReadFile
-{
-   ifstream input_file;
-   bool _eof;
-   bool closed;
-};
 
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
+
+class ReadFile
+{
+	private:
+		ifstream input_file;
+		bool _eof;
+		bool closed;
+	
+	public:
+		ReadFile* createReadFile(const char* file_name);
+		void destroyReadFile(ReadFile* rf);
+		String* readLine(ReadFile* rf);
+		bool eof(ReadFile* rf);
+		void close(ReadFile* rf);
+};
 
 #endif
